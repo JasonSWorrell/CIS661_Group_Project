f2b94c56624e2d7e1e3c33b019bd3ee980fe957f
WORRELL, JASON S TSgt USAF AMC 912 ARS/MXAA <jason.worrell@us.af.mil>
2023-10-14 23:05:56 -0700

    Add project files.

---
+++ Frontend/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Frontend
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
